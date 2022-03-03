aa2d67cee98b3519f0d0e12a2fcc31dc0ca558c5
Tiago Moraes <tiagoefmoraes@gmail.com>
2022-03-03 12:57:18 -0300

    remove elixir 1.5 to 1.9 from CI
    warning: the dependency :sourceror requires Elixir "~> 1.10"

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ExSamples.Mixfile do
     [
       app: :exsamples,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.10",
       description: description(),
       package: package(),
       deps: deps()
